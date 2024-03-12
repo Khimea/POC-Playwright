d594748d345e6d4da936b1c1b3cfd9d500e55178
Jerez Leonardo <73914243+Khimea@users.noreply.github.com>
2024-03-12 18:20:51 -0300

    update

--- support/TestHooks.js
+++ support/TestHooks.js
@@ -3,12 +3,13 @@ const page = require('@playwright/test')
 let {setDefaultTimeout} = require('@cucumber/cucumber')
 const path = require('path');
 setDefaultTimeout(60 * 15000)
-
+const isHeadless = process.env.HEADLESS;
+const headless = isHeadless ? true : false;
 require('dotenv').config({
     path: path.join(__dirname, '../.env'),
 });
 Before(async () => {
-    let browser = await page.chromium.launch({headless: true, ignoreHTTPSErrors: true})
+    let browser = await page.chromium.launch({headless: headless|| false, ignoreHTTPSErrors: true})
     global.browser = browser
     const context = await browser.newContext({
         viewport: { width: 1920, height: 1080 } // Maximizing screen by setting viewport size
