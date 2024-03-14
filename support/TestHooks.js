c0ed18fde57625514e68f6df7c6473159deff9b5
Jerez Leonardo <73914243+Khimea@users.noreply.github.com>
2024-03-14 10:10:20 -0300

    update

--- support/TestHooks.js
+++ support/TestHooks.js
@@ -1,22 +1,31 @@
-const {Before, AfterAll} = require('@cucumber/cucumber')
+const {Before, AfterAll, After} = require('@cucumber/cucumber')
 const page = require('@playwright/test')
 let {setDefaultTimeout} = require('@cucumber/cucumber')
+const utils = require("./utils");
+
 const path = require('path');
 setDefaultTimeout(60 * 15000)
-const isHeadless = process.env.HEADLESS;
-const headless = isHeadless ? true : false;
+const recordVideo = process.env.VIDEO || false ? true : false;
+const headless = process.env.HEADLESS || false ? true : false;
 require('dotenv').config({
     path: path.join(__dirname, '../.env'),
 });
 Before(async () => {
-    let browser = await page.chromium.launch({headless: headless|| false, ignoreHTTPSErrors: true})
+    let browser = await page.chromium.launch({headless: headless, ignoreHTTPSErrors: true})
     global.browser = browser
     const context = await browser.newContext({
-        viewport: { width: 1920, height: 1080 } // Maximizing screen by setting viewport size
+        viewport: { width: 1920, height: 1080 },
+        recordVideo: recordVideo ? { dir: './videos/' } : undefined
+
     })
     global.page = await context.newPage()
 })
 
+After(async (scenario) => {
+    await utils.takeScreenshot(scenario);
+  });
+
+  
 AfterAll(async () => {
     if (global.browser) {
         await global.browser.close();
