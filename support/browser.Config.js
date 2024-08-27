b0df780610132da0cae86655f348bae6ae01a53c
Jerez Leonardo <73914243+Khimea@users.noreply.github.com>
2024-08-27 17:28:36 -0300

    separo ejecucion mobile

--- support/browser.Config.js
+++ support/browser.Config.js
@@ -4,6 +4,7 @@ const headless = process.env.HEADLESS === 'true' ? true : false;
 const recordVideo = process.env.VIDEO === 'true' ? true : false;
 const browserType = process.env.BROWSER_TYPE || 'chromium';
 const device = process.env.DEVICE || 'chromium';
+const isMobile =  process.env.MOBILE === 'true' ? true : false;
 const config = require('../playwright.config');
 
 const getBrowser = async () => {
@@ -14,7 +15,9 @@ const getBrowser = async () => {
 };
 
 const configureContext = async () => {
-    let contextOptions = config.projects.find(project => project.name === device)?.use || {};
+    let devices = isMobile ? device : browserType;
+
+    let contextOptions = config.projects.find(project => project.name === devices)?.use || {};
     contextOptions.ignoreHTTPSErrors = true;
     if (recordVideo) {
         contextOptions.recordVideo = { dir: './videos/' };
